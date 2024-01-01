d91da696f8d883d43bb726318062bef4f3086845
HugoErb <erb.hugo@yahoo.fr>
2024-01-01 23:06:22 +0100

    fix polyfills + add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Importez votre component Home
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirection vers 'home'
+  { path: 'home', component: HomeComponent }, // Route pour 'home'
+];
